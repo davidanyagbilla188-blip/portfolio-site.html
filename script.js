113b6627f31d977454321d59734ec8e3023884ba
davidanyagbilla188-blip <davidanyagbilla188@gmail.com>
2026-01-07 00:13:39 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var messageButton = document.getElementById("messageBtn");
+var bgButton = document.getElementById("bgBtn");
+
+messageButton.onclick = function() {
+    alert("Hello! Thanks for visiting my portfolio website.");
+}
+
+bgButton.onclick = function() {
+    document.body.style.backgroundColor = "#e0f7fa";
+}
